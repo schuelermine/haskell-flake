8f25e639e157e9d1bde1d27a7362eebbcab347c1
Anselm Schüler <mail@anselmschueler.com>
2022-03-19 13:08:03 +0100

    Improve flake template

--- template/flake.nix
+++ template/flake.nix
@@ -1,5 +1,8 @@
 {
-  inputs.haskell-flake.url = "github:schuelermine/haskell-flake/b0";
+  inputs.haskell-flake = {
+    url = "github:schuelermine/haskell-flake/b0";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
   outputs = { self, nixpkgs, haskell-flake }:
     haskell-flake.lib.mkOutputs {
       inherit self nixpkgs;
